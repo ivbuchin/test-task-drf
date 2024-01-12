72b3e824d18ccaf337d810419085f30e3d07503c
Ivan Buchin <ivbuchin@gmail.com>
2024-01-12 07:36:22 +0500

    fix is_superuser

--- staff/signals.py
+++ staff/signals.py
@@ -5,7 +5,7 @@ from .models import Employee, Permission
 
 @receiver(pre_save, sender=Employee)
 def get_default_permission(sender, instance, **kwargs):
-    if instance.is_superuser:
+    if instance.is_admin:
       default_permission, created = Permission.objects.get_or_create(name='Admin')
       instance.permission = default_permission
 
