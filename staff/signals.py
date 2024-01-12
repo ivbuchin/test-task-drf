ee49052edfd8ba161755e10dcb1ef572b34d0e28
Ivan Buchin <ivbuchin@gmail.com>
2024-01-12 11:12:14 +0500

    return is_admin

--- staff/signals.py
+++ staff/signals.py
@@ -5,9 +5,9 @@ from .models import Employee, Permission
 
 @receiver(pre_save, sender=Employee)
 def get_default_permission(sender, instance, **kwargs):
-    if instance.is_admin():
-        default_permission, created = Permission.objects.get_or_create(name='Admin')
-        instance.permission = default_permission
+    if instance.is_admin :
+      default_permission, created = Permission.objects.get_or_create(name='Admin')
+      instance.permission = default_permission
 
 
 @receiver(post_save, sender=Employee)
